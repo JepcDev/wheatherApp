0893e238d665653b054991b9c189947c3d0d3020
Jepc <joseenrique.pc21@gmail.com>
2023-01-11 11:08:56 -0400

    ADD menu app

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -7,35 +7,19 @@ const optionQestion = [
   {
     type : 'list',
     name: 'option',
-    message: '¿ What do you want to do ?',
+    message: '¿ Que quieres hacer ?',
     choices: [
       {
-        value: '1',
-        name: `${'1.'.green} Crear tarea`
+        value: 1,
+        name: `${'1.'.green} Buscar ciudad o lugar`
       },
       {
-        value: '2',
-        name: `${'2.'.green} Listar tareas`
+        value: 2,
+        name: `${'2.'.green} Historial`
       },
       {
-        value: '3',
-        name: `${'3.'.green} Listar tareas completadas`
-      },
-      {
-        value: '4',
-        name: `${'4.'.green} Listar tareas pendientes`
-      },
-      {
-        value: '5',
-        name: `${'5.'.green} Completar tarea(s)`
-      },
-      {
-        value: '6',
-        name: `${'6.'.green} Borrar tarea`
-      },
-      {
-        value: '0',
-        name: `${'0.'.green}Salir`
+        value: 0,
+        name: `${'0.'.green} Salir`
       },
     ]
   }
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // console.log('Hola mundo');
-const { leerInput } = require('./helpers/inquirer');
+const { leerInput ,
+        inquirerMenu,
+        pause
+      } = require('./helpers/inquirer');
 
-const main = async() => {
-  const texto = await leerInput('País: ');
-  console.log(texto);
+console.clear();
+const main = async () => {
+  // const texto = await leerInput('País: ');
+  // console.log(texto);
+  let opt ;
+  do {
+    opt = await inquirerMenu();
+    console.log({opt});
+
+    if ( opt !== 0 ) await pause();
+
+  } while (opt !== 0);
 }
 
 main();
