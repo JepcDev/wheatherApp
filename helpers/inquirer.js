624b98944917dbfdc17dba4a8f51aa914ebf9d5e
Jepc <joseenrique.pc21@gmail.com>
2023-01-11 10:00:42 -0400

    ADD inquirer colors

---
+++ helpers/inquirer.js
@@ -0,0 +1,177 @@
+const inquirer = require('inquirer');
+// import inquirer from 'inquirer';
+require('colors');
+
+// Lista de opciones del usuario
+const optionQestion = [
+  {
+    type : 'list',
+    name: 'option',
+    message: '¿ What do you want to do ?',
+    choices: [
+      {
+        value: '1',
+        name: `${'1.'.green} Crear tarea`
+      },
+      {
+        value: '2',
+        name: `${'2.'.green} Listar tareas`
+      },
+      {
+        value: '3',
+        name: `${'3.'.green} Listar tareas completadas`
+      },
+      {
+        value: '4',
+        name: `${'4.'.green} Listar tareas pendientes`
+      },
+      {
+        value: '5',
+        name: `${'5.'.green} Completar tarea(s)`
+      },
+      {
+        value: '6',
+        name: `${'6.'.green} Borrar tarea`
+      },
+      {
+        value: '0',
+        name: `${'0.'.green}Salir`
+      },
+    ]
+  }
+];
+
+// Menu iterativo inquirer
+const inquirerMenu = async() => {
+  console.clear();
+  console.log('============================='.green);
+  console.log('    Seleccione una opción'.white);
+  console.log('============================='.green);
+
+  // aqui va la lista de las preguntas de la app
+  // devuelve el numero o valor de la opcion que elegimos
+  const {option}= await inquirer.prompt(optionQestion);
+  return option;
+}
+
+// Pone en pausa el prompt hasta que presionemos enter
+// DEV - pause
+const pause = async() => {
+  const question = [
+    {
+      type: 'input',
+      name: 'enter',
+      message: `Presione ${'ENTER'.green} para continuar`
+    }
+  ];
+
+  console.log('\n');
+  await inquirer.prompt(question);
+}
+
+// DEV - leerInput
+const leerInput = async(message)=> {
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      // message : mensaje
+      message,
+      validate(value){
+        if (value.length === 0) {
+          return 'Por favor ingrese un valor';
+        }
+        return true;
+      }
+    }
+  ];
+  const {desc} = await inquirer.prompt(question);
+  return desc;
+}
+
+// DEV listadoTareasBorrar
+const listadoTareasBorrar = async( tareas = [] ) => {
+  // map regresa un nuevo arreglo pero transforma los hijos del array
+  // es decir los valores del arreglo actal los tranforma a lo que yo quiera
+  const choices = tareas.map( (tarea, i) => {
+    const idx = `${i + 1}.`.green
+    // lo que retornemos es como van a lusir cada uno de los nuevos items del arreglo
+    return {
+      value: tarea.id,
+      name: `${idx} ${tarea.desc}`
+    }
+  });
+
+  choices.unshift({
+    value: '0',
+    name: '0.'.green + ' Cancelar'
+  });
+
+  const preguntas = [
+    {
+      type: 'list',
+      name: 'id',
+      message: 'Borrar',
+      choices
+    }
+  ]
+  const { id } = await inquirer.prompt(preguntas);
+  return id;
+  // {
+  //   value: tareas.id,
+  //   name:`${'1.'.green} Crear tarea`
+  // }
+}
+
+// DEV confirmation
+// Confirmar si se quiere borrar una tarea
+const confirmation = async(message) =>{
+
+  const question = [
+    {
+      type: 'confirm',
+      name: 'ok',
+      message
+    }
+  ];
+
+  const {ok} = await inquirer.prompt(question);
+  return ok;
+}
+
+// DEV mostrarListadoCheckList
+const mostrarListadoCheckList = async( tareas = [] ) => {
+  // map regresa un nuevo arreglo pero transforma los hijos del array
+  // es decir los valores del arreglo actal los tranforma a lo que yo quiera
+  const choices = tareas.map( (tarea, i) => {
+    const idx = `${i + 1}.`.green
+    // lo que retornemos es como van a lusir cada uno de los nuevos items del arreglo
+    return {
+      value: tarea.id,
+      name: `${idx} ${tarea.desc}`,
+      checked:(tarea.completadoEn)? true:false
+    }
+  });
+
+  // regresa un lista de tareas completadas
+  const pregunta = [
+    {
+      type: 'checkbox',
+      name: 'ids',
+      message: 'Selecciones',
+      choices
+    }
+  ]
+  const { ids } = await inquirer.prompt(pregunta);
+  return ids;
+}
+
+// DEV - exports
+module.exports = {
+  inquirerMenu,
+  pause,
+  leerInput,
+  listadoTareasBorrar,
+  confirmation,
+  mostrarListadoCheckList
+}
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hola mundo');
+// console.log('Hola mundo');
+const { leerInput } = require('./helpers/inquirer');
+
+const main = async() => {
+  const texto = await leerInput('País: ');
+  console.log(texto);
+}
+
+main();
