c0d269aad7369c69dd4d43e3128f05d5eaaac401
Jepc <joseenrique.pc21@gmail.com>
2023-01-21 17:09:45 -0400

    MapBox Search API and Access token MapBox

--- models/busquedas.js
+++ models/busquedas.js
@@ -13,7 +13,8 @@ class Busquedas {
   // Vamos a realizar la peticion http y extraer o traer la informacion
     // console.log('ciudad',lugar);
     try {
-      const resp = await axios.get('https://reqres.in/api/users?page=2');
+      // const resp = await axios.get('https://reqres.in/api/users?page=2');
+      const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?limit=5&language=es&access_token=pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2tvZHh4Y3YwMDhnMnBvY3ozbHUxdGJvIn0.3zptKSSxJrM5VmfjnkKMYA');
       console.log(resp.data);
 
       return []; //reotnar una lista o array con los lugares  que coincidan con la palabra o lugar que escribio el usuario
