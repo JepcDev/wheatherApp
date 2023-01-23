c53e35a1d493c3ccf0e8471588e34c9a9fc872be
Jepc <joseenrique.pc21@gmail.com>
2023-01-23 10:56:55 -0400

    ADD instantiate axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -8,16 +8,38 @@ class Busquedas {
     //TODO: leer db si existe
   }
 
+  get paramsMapbox() {
+    return {
+      // access_token:
+      'access_token' :'pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2tvZHh4Y3YwMDhnMnBvY3ozbHUxdGJvIn0.3zptKSSxJrM5VmfjnkKMYA',
+      'limit': 5,
+      'language': 'es'
+    }
+  }
+
   // Es async por va a recibir informacion de una API
   async ciudad( lugar = ''){
-  // Vamos a realizar la peticion http y extraer o traer la informacion
-    // console.log('ciudad',lugar);
     try {
+      // Vamos a realizar la peticion http y extraer o traer la informacion
+      // console.log('ciudad',lugar);
       // const resp = await axios.get('https://reqres.in/api/users?page=2');
-      const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?limit=5&language=es&access_token=pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2tvZHh4Y3YwMDhnMnBvY3ozbHUxdGJvIn0.3zptKSSxJrM5VmfjnkKMYA');
+      // const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?limit=5&language=es&access_token=pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2tvZHh4Y3YwMDhnMnBvY3ozbHUxdGJvIn0.3zptKSSxJrM5VmfjnkKMYA');
+      // Todo lo demas que viene despues de un signo de interrogacion en una peticcion url es conocidos como params
+      const instance = axios.create({
+        baseURL:`https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+        params: this.paramsMapbox
+        // params: {
+          // access_token:
+        //   'access_token' :'pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2tvZHh4Y3YwMDhnMnBvY3ozbHUxdGJvIn0.3zptKSSxJrM5VmfjnkKMYA',
+        //   'limit': 5,
+        //   'language': 'es'
+        // }
+      });
+
+      const resp = await instance.get();
       console.log(resp.data);
 
-      return []; //reotnar una lista o array con los lugares  que coincidan con la palabra o lugar que escribio el usuario
+      return []; //retonar una lista o array con los lugares  que coincidan con la palabra o lugar que escribio el usuario
     } catch (error) {
       return [];
     }
