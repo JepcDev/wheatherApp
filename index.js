5bbf32a10cf691d0b702e4b44fa6592fbff3f247
Jepc <joseenrique.pc21@gmail.com>
2023-01-26 11:33:48 -0400

    ADD saveDB FIX:idSeleccion=0

--- index.js
+++ index.js
@@ -30,17 +30,22 @@ const main = async () => {
 
         // El usuario tiene que seleccionar un lugar de la lista de lugares coincidentes
         const idSeleccion = await listarLugares(lugares);
+        // Previene cuando el id es 0
+        if (idSeleccion === '0') continue;
+
         // Extraer el lugar seleccionado
         // console.log({idSeleccion});
         // find(callback) regresa el primer elemento que cumpla la condicion que especificamos en su callback
         const lugarSeleccion = lugares.find(l => l.id === idSeleccion);
         // console.log(lugarSeleccion);
+        // Guardar en DB
+        busquedas.agregarHidtorial(lugarSeleccion.nombre);
 
         // Una vez el usuario selecciona un lugar obtenemos los datos del clima relacionados al geoLocation de ese lugar
         const clima = await busquedas.weatherPlace(lugarSeleccion.lat, lugarSeleccion.lng);
         // Mostrar resultados de la busquedas
         console.clear();
-        console.log('\n Información de la ciudad\n'.green);
+        console.log('\nInformación de la ciudad\n'.green);
         console.log('Ciudad: ', lugarSeleccion.nombre.green);
         console.log('Lat: ', lugarSeleccion.lat);
         console.log('Lng: ', lugarSeleccion.lng);
@@ -50,6 +55,11 @@ const main = async () => {
         console.log('Como esta el clima: ', clima.desc.green);
       break;
 
+      case 2:
+        busquedas.historial.forEach( (lugar, i)=>{
+          const idx = `${i+1}.`.green;
+          console.log(`${idx} ${lugar}`);
+        });
       default:
         break;
     }
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,11 +1,14 @@
+const fs = require('fs');
+
 const axios = require('axios');
 
 // Process contiene todo lo que son nuestras variables de entorno
 // console.log(process.env);
+// DEV Busquedas
 class Busquedas {
   // historial = ['Teguciagalpa', 'Madrid', 'San José'];
   historial = [];
-
+  dbPat = './db/data.json';
   constructor(){
     //TODO: leer db si existe
   }
@@ -63,14 +66,16 @@ class Busquedas {
   }
 
   // https://api.openweathermap.org/data/2.5/weather?lat=37.336191&lon=-121.890583&appid=58e06af4ed842ec91c0321d6c2942dd5&units=metric&lang=es
-  // Obtener el clima del lugar
+  //DEV weatherPlace Obtener el clima del lugar
+  // Cuando la parametro que nos envian tiene el mismo nombre que el parametro o atributo de la funcion no es necesario ponerlo de nuevo
   async weatherPlace( lat = '', lon = '' ){
     try {
+      // create es de axios
       const instance = axios.create({
         baseURL: `https://api.openweathermap.org/data/2.5/weather`,
         params: { ...this.paramsOpenWeather, lat, lon}
         // params:{
-        //   lat,
+        //   lat,//parametro con el mismo valor que el parametro que nos envian
         //   lon,
         //   appid: process.env.OPENWEATHER_KEY,
         //   units: 'metric',
@@ -78,8 +83,10 @@ class Busquedas {
         // }
       });
 
+      // get es de axios
       const resp = await instance.get();
-      console.log(resp);
+      // console.log(resp);
+      // data es de axios
       const { weather, main } = resp.data;
 
       return {
@@ -91,6 +98,35 @@ class Busquedas {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  // DEV agregarHidtorial
+  agregarHidtorial(lugar = ''){
+    // TODO: prevenir duplicados
+    if (this.historial.includes(lugar.toLocaleLowerCase())) {
+      return;
+    }
+    // añadir un lugar al historial - unshift para ponerlo al inicio
+    this.historial.unshift(lugar.toLocaleLowerCase())
+
+    // TODO: grabar en una DB
+    this.saveDB();
+
+  }
+
+  // DEv saveDB
+  saveDB(){
+
+    const payload = {
+      historial: this.historial
+    };
+
+    fs.writeFileSync( this.dbPat, JSON.stringify(payload) );
+  }
+
+  // DEV readDB
+  readDB(){
+
   }
 }
 
